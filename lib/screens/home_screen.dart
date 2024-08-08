1012a40d526ef7c1dba5a5dbf52b358cffd740ca
leesehwi <leesehwi@minigate.net>
2024-08-08 17:31:26 +0900

    Hell

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_tabbar/dynamic_tabbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_lover_tale/screens/initial/info_insert_screen.dart';
 import 'package:flutter_lover_tale/screens/auth/login_screen.dart';
@@ -20,6 +21,43 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool isScrollable = false;
+  bool showNextIcon = false;
+  bool showBackIcon = false;
+
+  List<TabData> tabs = [
+    TabData(
+      index: 1,
+      title: const Tab(
+        child: Text('홈'),
+      ),
+      content: Container(
+        color: const Color.fromRGBO(245, 245, 245, 1),
+      ),
+    ),
+    TabData(
+      index: 2,
+      title: const Tab(
+        child: Text('이야기'),
+      ),
+      content: const Center(child: Text('Content for Tab 2')),
+    ),
+    TabData(
+      index: 2,
+      title: const Tab(
+        child: Text('추억지도'),
+      ),
+      content: const Center(child: Text('Content for Tab 2')),
+    ),
+    TabData(
+      index: 2,
+      title: const Tab(
+        child: Text('내정보'),
+      ),
+      content: const Center(child: Text('Content for Tab 2')),
+    ),
+    // Add more tabs as needed
+  ];
   // ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
   // ┃★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆┃
   // ┃★☆★☆★☆★☆★☆★☆★☆★☆★☆화면★☆★☆★☆★☆★☆★☆★☆★☆┃
@@ -67,8 +105,30 @@ class _HomeScreenState extends State<HomeScreen> {
                     IconButton(onPressed: (){}, icon: const Icon(Icons.more_horiz))
                   ],
                 ),
-                body: Container(
-                  color: const Color.fromRGBO(245, 245, 245, 1),
+                body: SizedBox(
+                  width: mq.width,
+                  height: mq.height,
+                  child: Column(
+                   children: [
+                     Expanded(
+                       child: DynamicTabBarWidget(
+                         dividerHeight: 0,
+                         labelColor: const Color.fromRGBO(109, 109, 109, 1),
+                         unselectedLabelColor: const Color.fromRGBO(197, 197, 197, 1),
+                         indicatorColor: const Color.fromRGBO(109, 109, 109, 1),
+                         dynamicTabs: tabs,
+                         isScrollable: isScrollable,
+                         onTabControllerUpdated: (controller) {
+                         },
+                         onTabChanged: (index) {
+                         },
+                         onAddTabMoveTo: MoveToTab.last,
+                         showBackIcon: showBackIcon,
+                         showNextIcon: showNextIcon,
+                       ),
+                     ),
+                   ],
+                  ),
                 ),
                 floatingActionButton: FloatingActionButton(
                   child: const Text("로그아웃임시"),
